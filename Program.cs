65ec951b0ff1f97cff8322f53383dd4cc6612586
RehkMansa <godspowernathaniel25@gmail.com>
2022-08-09 15:16:50 +0100

    created a txt file for calculator

--- Program.cs
+++ Program.cs
@@ -5,46 +5,6 @@ public class Program
 {
   public static void Main(string[] args)
   {
-    double num1, num2;
-
-    string operation;
-
-
-    Console.WriteLine("Rehkmansa calculator");
-    Console.Write("\nEnter Your First Number: ");
-
-
-    num1 = Convert.ToDouble(Console.ReadLine());
-
-    Console.Write("\nEnter Your Second Number: ");
-
-    num2 = Convert.ToDouble(Console.ReadLine());
-
-
-
-    Console.Write("\nEnter operation symbol (+ or - or / or *) to perform required operation ");
-
-
-    operation = Console.ReadLine();
-
-    switch (operation)
-    {
-      case "+":
-        Console.WriteLine(num1 + num2);
-        break;
-
-      case "-":
-        Console.WriteLine(num1 - num2);
-        break;
-      case "/":
-        Console.WriteLine(num1 / num2);
-        break;
-      case "*":
-        Console.WriteLine(num1 * num2);
-        break;
-      default:
-        Console.WriteLine("Wrong Operand Type");
-        break;
-    }
+    
   }
 }
