4c56852a1bad1256167bf76b6ea0443ff6a1c497
RehkMansa <godspowernathaniel25@gmail.com>
2022-08-09 15:22:09 +0100

    created array type

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ public class Program
 {
   public static void Main(string[] args)
   {
-    
+    //Arrays
+
+    int[] primeNumbers = {1, 2, 3, 5, 6};
+
+    Console.WriteLine(primeNumbers[]);
   }
 }
