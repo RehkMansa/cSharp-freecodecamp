a22ee96a164bb1b32d367c4871ca35244c69e1b8
RehkMansa <godspowernathaniel25@gmail.com>
2022-08-11 00:14:58 +0100

    added getters and setters

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ public class Program
 {
   public static void Main(string[] args)
   {
-    Users rehkmansa = new Users("rehkmansa", "developer", 22);
+    Users rehkmansa = new Users("rehkmansa", "developer", 22, "hjka");
+
+
 
 
     Console.WriteLine(rehkmansa.UserRole());
--- Users.cs
+++ Users.cs
@@ -1,15 +1,39 @@
 class Users
 {
   public string name; public string role; public int age;
-  public Users(string _name, string _role, int _age)
+  private string officeRole = "";
+  public Users(
+    string _name,
+    string _role,
+    int _age,
+    string _officeRole)
   {
     name = _name;
     role = _role;
     age = _age;
+    OfficeRole = _officeRole;
   }
-
   public string UserRole()
   {
-    return name + " has the role of " + role;
+    return name + " has the role of " + officeRole;
   }
+
+  public string OfficeRole
+  {
+    get { return officeRole; }
+
+    set
+    {
+      if (value == "engineering" || value == "design" || value == "product")
+      {
+        officeRole = value;
+      }
+      else
+      {
+        officeRole = "others";
+      }
+    }
+  }
+
+
 }
