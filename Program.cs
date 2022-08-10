043d83e8249dd80c612737cf4226648d1f188f3e
RehkMansa <godspowernathaniel25@gmail.com>
2022-08-10 21:30:04 +0100

    guessing game

--- Program.cs
+++ Program.cs
@@ -9,11 +9,36 @@ public class Program
 
     string guess = "";
 
-    while (guess != secret.ToLower())
+    int guessCount = 5;
+
+    bool outOfGuess = false;
+
+    while (guess != secret && !outOfGuess)
     {
-      Console.Write("Enter a word: ");
+      guessCount--;
+
+      Console.Write("Please enter any word: ");
 
       guess = Console.ReadLine().ToLower();
+
+      Console.WriteLine("You have " + guessCount + " left");
+
+      if (guessCount <= 0)
+      {
+        outOfGuess = true;
+      }
+
     }
+
+
+    if (outOfGuess)
+    {
+      Console.WriteLine("You loose");
+    }
+    else
+    {
+      Console.WriteLine("Yeah!!! You got the answer");
+    }
+
   }
 }
